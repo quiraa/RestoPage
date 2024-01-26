bb2217c125da7221c2efb8ab7de4481452e29b7f
aryadzkr <109120590+quiraa@users.noreply.github.com>
2024-01-27 05:20:55 +0700

    adding media queries

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.add("active");
+  navMenu.classList.add("active");
+});
+
+navMenu.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+});
